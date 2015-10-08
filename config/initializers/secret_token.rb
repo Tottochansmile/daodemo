76c1a7e0a7249e7fdf7af2b37f07f83b7f36bc19
Tottochansmile <daopt22@gmail.com>
2015-10-08 14:41:46 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DaoApp::Application.config.secret_key_base = 'd0f865fbe54ceace3dfead3f1fd29a713b1dd9c6947b504351e63f94cccb69f7a92bdfb29cd00ef5a63cfce2fabb766fc4d379051b6e451a80ddfce15a73d15d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DaoApp::Application.config.session_store :cookie_store, key: '_dao_app_session'
